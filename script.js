b55a0c36d0e0e9b41a997cb4e4f0c765637d1191
Jasur <xecopubgm@gmail.com>
2024-11-13 16:57:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// JavaScript kodlari kerak bo'lsa, shu yerda yozing.
-function showPhoneNumber() {
-    const phoneNumber = document.getElementById('phone-number');
-    phoneNumber.style.display = (phoneNumber.style.display === 'none') ? 'block' : 'none';
-}
+
